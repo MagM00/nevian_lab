919fbb08ab9e5b90a3845cabbbde889d4cf06bc3
Jun Huang <44811884+junhuang7@users.noreply.github.com>
2024-08-21 11:57:57 +0200

    led trigger button added

---
+++ exp_control/led_trigger_simple/led_trigger_simple.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(7, OUTPUT); // Set the built-in LED as output
+  digitalWrite(7, HIGH); // Turn the LED on
+  delay(1000); // Wait for 1800000 milliseconds (1 second)
+  digitalWrite(7, LOW); // Turn the LED off
+}
+
+void loop() {
+  // Do nothing, or you could implement an infinite loop
+  // while(1) {}
+}
